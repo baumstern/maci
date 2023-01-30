2fab6f9e19543a99735b6e216be7701e8531a959
Daehyun Paik <paik@a30a.dev>
2023-01-30 18:14:27 +0100

    wip

---
+++ index.ts
@@ -0,0 +1,23 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+// TODO: generate new IAM to CI builder role
+const web = new aws.ec2.Instance("maci-builder", {
+    ami: "ami-08d487b452907cccd",
+    instanceType: "z1d.6xlarge",
+    tags: {
+        Name: "maci-builder",
+    },
+    vpcSecurityGroupIds: ["sg-0aea3cbb15e30a921"],
+    subnetId: "subnet-0817be1b2160793b5",
+    associatePublicIpAddress: true,
+    keyName: "maci-devops",
+    rootBlockDevice: {
+        volumeSize: 128,
+        volumeType: "gp3"
+    }
+});
+
+
+export const publicIP = web.publicIp

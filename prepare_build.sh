3906f1bd2f303c63f720fb694cee2dd499c82593
Daehyun Paik <paik@a30a.dev>
2023-01-31 11:28:23 +0100

    wip

--- prepare_build.sh
+++ prepare_build.sh
@@ -4,9 +4,16 @@ BASE_DIR="$( cd "$( dirname "$0" )" && pwd )"
 MACI_DIR="$HOME"/maci
 
 NPM="/home/ubuntu/.nvm/versions/node/v16.19.0/bin/npm"
+NPX="/home/ubuntu/.nvm/versions/node/v16.19.0/bin/npx"
 NODE="/home/ubuntu/.nvm/versions/node/v16.19.0/bin/node"
 
-export PATH="$NODE:$PATH"
+export PATH="$NODE:$NPX:$NPM:$PATH"
+
+sudo ln -s "$(which node)" /usr/bin/node
+sudo ln -s "$(which npm)" /usr/bin/npm
+
+
+rm -rf "$MACI_DIR"/cli/zkeys
 
 # Setup maci repo
 cd "$MACI_DIR"

6cac5b00f9a0fd6b53653e84b0306f366a54d2ab
Your Name <you@example.com>
2023-06-28 22:47:20 +0000

    Add zkey

--- cli/test_coordinator-service.sh
+++ cli/test_coordinator-service.sh
@@ -4,9 +4,15 @@ set -e
 
 BASE_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+mkdir ./zkeys
+wget -q -P ./zkeys https://maci-develop-fra.s3.eu-central-1.amazonaws.com/v1.1.1-aa4ba27/10-2-1-2/ProcessMessages_10-2-1-2_test.0.zkey
+wget -q -P ./zkeys https://maci-develop-fra.s3.eu-central-1.amazonaws.com/v1.1.1-aa4ba27/10-2-1-2/TallyVotes_10-1-2_test.0.zkey
 
 
-docker run --detach --platform linux/amd64 -p 8080:8080 ghcr.io/privacy-scaling-explorations/maci-coordinator:main
+# docker run --detach --platform linux/amd64 -p 8080:8080 ghcr.io/privacy-scaling-explorations/maci-coordinator:main
+
+# compileSol
+
 
 
 "$BASE_DIR"/../.github/scripts/run-e2e-tests.sh
